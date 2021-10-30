f2cdd6b43c667dffa24115437c30233f65a5c1eb
Sheldon Malboeuf <sheldonm07@gmail.com>
2021-10-30 19:50:14 -0400

    Add new options for customizing character

--- Assets/Scripts/Character/Customization/AppearanceChanger.cs
+++ Assets/Scripts/Character/Customization/AppearanceChanger.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class AppearanceChanger : MonoBehaviour
 {
-    public SpriteRenderer customizablePart;
+    public Image customizablePart;
+    [SerializeField] private string optionTextPrefix = "";
+    [SerializeField] private TextMeshProUGUI optionText;
 
     public List<Sprite> options = new List<Sprite>();
 
     private int currentOption = 0;
+
+    private void Start()
+    {
+        customizablePart.sprite = options[currentOption];
+        optionText.text = GetOptionText(currentOption);
+    }
 
     public void NextOption()
     {
@@ -19,6 +29,7 @@ public class AppearanceChanger : MonoBehaviour
         }
 
         customizablePart.sprite = options[currentOption];
+        optionText.text = GetOptionText(currentOption);
     }
 
     public void PreviousOption()
@@ -31,5 +42,11 @@ public class AppearanceChanger : MonoBehaviour
         }
 
         customizablePart.sprite = options[currentOption];
+        optionText.text = GetOptionText(currentOption);
+    }
+
+    public string GetOptionText(int optionNumber)
+    {
+        return $"{optionTextPrefix} #{optionNumber + 1}";
     }
 }
